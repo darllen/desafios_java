8534e77b990b20a3590b97325685de95f64f9c8d
Jamilly Anunciada <jamillymilly10@hotmail.com>
2021-09-23 21:22:23 -0300

    Add files via upload

--- calcular-imc/imc.java
+++ calcular-imc/imc.java
@@ -9,50 +9,54 @@ public class imc {
 		Scanner leia = new Scanner(System.in);
 		DecimalFormat deci = new DecimalFormat("0.0");
 		double peso, imc, quilos, pesoIdeal;
-		final double altura;
+		double altura;
+		String opcao = "S";
 
-		System.out.print("Peso em Kg: ");
-		peso = leia.nextDouble();
-		System.out.print("Altura em m: ");
-		altura = leia.nextDouble();
+		while (opcao == "S"){
+			System.out.print("Peso em Kg: ");
+			peso = leia.nextDouble();
+			System.out.print("Altura em m: ");
+			altura = leia.nextDouble();
 
-		imc = (peso / Math.pow(altura, 2));
-		System.out.println("\nIMC =  " + deci.format(imc));
+			imc = (peso / Math.pow(altura, 2));
+			System.out.println("\nIMC =  " + deci.format(imc));
 
-		if (imc < 18.5) {
-			System.out.println("\nSituação: Abaixo do peso");
-			pesoIdeal = 19 * Math.pow(altura, 2);
-			quilos = pesoIdeal - peso;
-			System.out.println("Para peso ideal faltam " + deci.format(quilos) + " Kg");
+			if (imc < 18.5) {
+				System.out.println("\nSituação: Abaixo do peso");
+				pesoIdeal = 19 * Math.pow(altura, 2);
+				quilos = pesoIdeal - peso;
+				System.out.println("Para peso ideal faltam " + deci.format(quilos) + " Kg");
 
-		} else if ((imc > 18.9) && (imc < 24.9)) {
-			System.out.println("\nSituação: Peso ideal");
+			} else if ((imc > 18.9) && (imc < 24.9)) {
+				System.out.println("\nSituação: Peso ideal");
 
-		} else if ((imc > 25) && (imc < 29.9)) {
-			System.out.println("\nSituação: Sobrepeso");
-			pesoIdeal = 24.8 * Math.pow(altura, 2);
-			quilos = peso - pesoIdeal;
-			System.out.println("Para peso ideal perder " + deci.format(quilos) + " Kg");
+			} else if ((imc > 25) && (imc < 29.9)) {
+				System.out.println("\nSituação: Sobrepeso");
+				pesoIdeal = 24.8 * Math.pow(altura, 2);
+				quilos = peso - pesoIdeal;
+				System.out.println("Para peso ideal perder " + deci.format(quilos) + " Kg");
 
-		} else if ((imc > 30) && (imc < 34.9)) {
-			System.out.println("\nSituação: Obesidade grau I");
-			pesoIdeal = 24.8 * Math.pow(altura, 2);
-			quilos = peso - pesoIdeal;
-			System.out.println("Para peso ideal perder " + deci.format(quilos) + " Kg");
+			} else if ((imc > 30) && (imc < 34.9)) {
+				System.out.println("\nSituação: Obesidade grau I");
+				pesoIdeal = 24.8 * Math.pow(altura, 2);
+				quilos = peso - pesoIdeal;
+				System.out.println("Para peso ideal perder " + deci.format(quilos) + " Kg");
 
-		} else if ((imc > 35) && (imc < 39.9)) {
-			System.out.println("\nSituação: Obesidade severa grau II");
-			pesoIdeal = 24.8 * Math.pow(altura, 2);
-			quilos = peso - pesoIdeal;
-			System.out.println("Para peso ideal perder " + deci.format(quilos) + " Kg");
+			} else if ((imc > 35) && (imc < 39.9)) {
+				System.out.println("\nSituação: Obesidade severa grau II");
+				pesoIdeal = 24.8 * Math.pow(altura, 2);
+				quilos = peso - pesoIdeal;
+				System.out.println("Para peso ideal perder " + deci.format(quilos) + " Kg");
 
-		} else if (imc >= 40) {
-			System.out.println("\nSituação: Obesidade mórbida grau III");
-			pesoIdeal = 24.8 * Math.pow(altura, 2);
-			quilos = peso - pesoIdeal;
-			System.out.println("Para peso ideal perder " + deci.format(quilos) + " Kg");
+			} else if (imc >= 40) {
+				System.out.println("\nSituação: Obesidade mórbida grau III");
+				pesoIdeal = 24.8 * Math.pow(altura, 2);
+				quilos = peso - pesoIdeal;
+				System.out.println("Para peso ideal perder " + deci.format(quilos) + " Kg");
+			}
+			System.out.print("\nCalcular novamente? [S/N] ");
+			opcao = leia.next();
 		}
-
 	}
 
 }
