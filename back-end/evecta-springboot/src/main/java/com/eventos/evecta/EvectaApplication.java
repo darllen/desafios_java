ac90a921673a854b25a30ebb8ccb7c0f815a4f4a
Jamilly Anunciada <jamillymilly10@hotmail.com>
2021-12-07 17:45:12 -0300

    subindo projeto aprendizado spring

---
+++ back-end/evecta-springboot/src/main/java/com/eventos/evecta/EvectaApplication.java
@@ -0,0 +1,13 @@
+package com.eventos.evecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvectaApplication.class, args);
+	}
+
+}
