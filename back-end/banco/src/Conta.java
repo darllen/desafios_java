24fcaadbadfb5b3018f63dea2e025b0079e53360
Jamilly Anunciada <jamillymilly10@hotmail.com>
2022-02-09 15:19:22 -0300

    aplicando o conceito de herança

---
+++ back-end/banco/src/Conta.java
@@ -0,0 +1,24 @@
+public class Conta {
+
+    protected double saldo;
+    public int conta;
+
+
+    public void sacar(double valor){
+        if (this.saldo >= valor){
+            this.saldo -= valor;
+        }else{
+            System.out.println("Saldo Insuficiente");
+        }
+    }
+
+    public void depositar(double valor){
+        this.saldo += valor;
+    }
+
+    public void verSaldo(){
+        System.out.println("Saldo R$ "+ this.saldo);
+
+    }
+
+}
--- back-end/banco/src/ContaCorrente.java
+++ back-end/banco/src/ContaCorrente.java
@@ -1,26 +1,12 @@
-public class ContaCorrente {
+public class ContaCorrente extends Conta{
+    // ContaCorrente herda de Conta
 
-    private double saldo;
-    public int conta;
+    //Reescrever o método da classe mãe
 
-
-    public void sacar(double valor){
-       if (this.saldo >= valor){
-           this.saldo -= valor;
-           this.juro();
-       }else{
-           System.out.println("Saldo Insuficiente");
-       }
-    }
-
-    public void depositar(double valor){
-        this.saldo += valor;
-    }
-
-    public void verSaldo(){
+    @Override
+    public void sacar(double valor) {
+        super.sacar(valor);
         this.juro();
-        System.out.println("Saldo R$ "+ this.saldo);
-
     }
 
     private void juro(){
