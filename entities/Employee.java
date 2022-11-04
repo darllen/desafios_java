13af822515e0e71c4d480d780598941ea1cbea23
Jamilly Anunciada <jamillymilly10@hotmail.com>
2022-11-04 18:36:02 -0300

    Update Employee.java

--- entities/Employee.java
+++ entities/Employee.java
@@ -1,18 +1,50 @@
 package entities;
 
 public class Employee {
-    public String name;
-    public double grossSalary;
-    public double tax;
 
-    public double netSalary() {
-        return this.grossSalary - this.tax;
+    private Integer id;
+    private String name;
+    private Double salary;
+
+    public Employee() {
     }
 
-    public void increaseSalary(double porcent){
-        this.grossSalary += (this.grossSalary*porcent)/100;
+    public Employee(Integer id, String name, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double percentage) {
+        salary += salary * percentage / 100.0;
+    }
+
     public String toString() {
-        return this.name + ", $ " + String.format("%.2f", netSalary());
+        return id + ", " + name + ", " + String.format("%.2f", salary);
     }
 }
+
