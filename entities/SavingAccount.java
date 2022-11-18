79f246e8723d384d3f2a4d8d92c1d5ee1d83dbac
Jamilly Anunciada <jamillymilly10@hotmail.com>
2022-11-18 19:47:12 -0300

    business rule
    Estudando sobre herança, sobreposição e @Override

---
+++ entities/SavingAccount.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class SavingAccount extends Account {
+    private Double interestRate;
+
+
+    public SavingAccount() {
+        super();
+    }
+
+    public SavingAccount(Integer number, String holder, Double balance, Double interestRate) {
+        super(number, holder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public Double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(Double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void updateBalace(){
+        balance += balance * interestRate;
+    }
+
+    @Override
+    public void withDraw(double amount){
+        balance -= amount;
+    }
+
+}
